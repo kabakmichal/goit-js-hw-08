91f46799a9e88c4db5edc49d83dc8fe4995c794b
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-07-29 16:25:20 +0000

    Deploying to gh-pages from @ kabakmichal/goit-js-hw-08@a491b1062b6b98f91b3a0f0393c3de61e4446684 🚀

--- 02-video.22b01899.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector("iframe"),n=new Vimeo.Player(e).getCurrentTime().then((function(e){console.log(e)})).catch((function(e){}));console.log(n)}();
-//# sourceMappingURL=02-video.22b01899.js.map
--- 02-video.68d6735d.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),o=new Vimeo.Player(e).getCurrentTime().then((function(e){console.log(e)})).catch((function(e){}));console.log(o);
-//# sourceMappingURL=02-video.68d6735d.js.map
