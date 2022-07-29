40d9ffc0d2bcbb8478b82a2342b84904be9311aa
kabakmichal <99827389+kabakmichal@users.noreply.github.com>
2022-07-29 15:27:34 +0200

    test

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,31 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+console.log('podlaczone');
+
+// player.addEventListener('blur', () => {
+//     console.log('dziala');
+//     player
+//       .getCurrentTime()
+//       .then(function (seconds) {
+//         // seconds = the current playback position
+//         console.log(seconds);
+//       })
+//       .catch(function (error) {
+//         // an error occurred
+//       });
+// })
+
+
+///////////////////////////////////////////////////
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// player.on('play', function () {
+//   console.log('played the video!');
+// });
+
+// player.getVideoTitle().then(function (title) {
+//   console.log('title:', title);
+// });
+////////////////////////////////
